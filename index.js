71d66e22244e7c57393067ef5e7d13f80d686fbf
unknown <srueda27@gmail.com>
2018-06-04 09:12:50 -0500

    Header con sombras

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+// import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+// Create a component
+const App = () => (
+    <Header text={'Albums'} />
+);
+
+/* Las llaves se pueden eliminar dado que no es necesario poner el return
+  xq la Fat Arrow Function implicitamente está diciendo que hay un return
+const App = () => {
+  return (
+      <Text>Some Text rigth here</Text>
+    );
+}
+
+*/
+
+// Render it to the device
 AppRegistry.registerComponent('albums2', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+// import libraries for making the component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.text}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    elevation: 2,
+    position: 'relative'
+  },
+
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// make the component available to other parts of the app
+export default Header;
